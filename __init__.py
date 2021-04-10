2a4af320657d24c3d34abfdd776cbc3c43380bea
Tom-the-Bomb <thello522@gmail.com>
2021-04-10 15:27:39 -0400

    modified setup

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
 from Discord_Games.Discord_Games import hangman, tictactoe, twenty_48, ChessGame, connect_four
+
+__version__ = "1.0.3"
+__author__  = "Tom-the-Bomb"
+__license__ = "MIT"
+__copyright = "Copyright 2021 Tom-the-Bomb"
+
+__all__     = ["hangman", "tictactoe", "twenty_48", "ChessGame", "connect_four"]
---
+++ setup.py
@@ -0,0 +1,35 @@
+from   setuptools import setup
+import re
+
+with open("__init__.py") as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+setup(
+    name         = "Discord_Games", 
+    author       = "Tom-the-Bomb", 
+    version      = version, 
+    description  = "A library to help users easily implement games within their discord bot",
+    long_description              = open("README.md").read(),
+    long_description_content_type = "text/markdown",
+    license      = "MIT",
+    url          = "https://github.com/Tom-the-Bomb/Discord-Games",
+    project_urls = {
+        "Repository"   : "https://github.com/Tom-the-Bomb/Discord-Games",
+        "Issue tracker": "https://github.com/Tom-the-Bomb/Discord-Games/issues",
+    },
+    classifiers  = [
+        "Intended Audience :: Developers",
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+    include_package_data = True,
+    packages             = ['Discord_Games'],
+    zip_safe        = False,
+    python_requires = '>=3.7'
+)
