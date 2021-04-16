648bcdff4af6f06aa4a83314218a4b66ef791d06
Tom-the-Bomb <thello522@gmail.com>
2021-04-16 09:54:08 -0400

    smh

--- Discord_Games/hangman.py
+++ Discord_Games/hangman.py
@@ -146,7 +146,7 @@ class Hangman:
         self._embed.description = f"```\n{stages[self._counter]}\n```"
         self._embed.color = color
         self._embed.add_field(name='Word', value=f"{' '.join(self.correct)}")
-        wrong_letters = ', '.join(self.wrong_letters) or ' - \u200b'
+        wrong_letters = ', '.join(self.wrong_letters) or '  \u200b'
         self._embed.add_field(name='Guessed letters', value=wrong_letters)
         self._embed.add_field(name='Lives left', value=self.lives(), inline=False)
         self._message = await ctx.send(embed=self._embed, **kwargs)
