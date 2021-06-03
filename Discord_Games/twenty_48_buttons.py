b4f77149932151d50e4b8b7c556ca8f0de3898a4
Tom-the-Bomb <thello522@gmail.com>
2021-06-02 20:24:17 -0400

    e

--- Discord_Games/twenty_48_buttons.py
+++ Discord_Games/twenty_48_buttons.py
@@ -7,7 +7,8 @@ from .twenty_48 import Twenty48
 
 class Twenty48_Button(discord.ui.Button['Twenty48']):
     
-    def __init__(self, emoji: str):
+    def __init__(self, game, emoji: str):
+        self.game = game
         super().__init__(style=discord.ButtonStyle.blurple, label=emoji)
 
     async def callback(self, interaction: discord.Interaction):
@@ -17,19 +18,19 @@ class Twenty48_Button(discord.ui.Button['Twenty48']):
         emoji = self.label
 
         if emoji == '➡️':
-            await self.view.MoveRight()
+            await self.game.MoveRight()
 
         elif emoji == '⬅️':
-            await self.view.MoveLeft()
+            await self.game.MoveLeft()
 
         elif emoji == '⬇️':
-            await self.view.MoveDown()
+            await self.game.MoveDown()
 
         elif emoji == '⬆️':
-            await self.view.MoveUp()
+            await self.game.MoveUp()
 
-        await self.view.spawn_new()
-        BoardString = await self.number_to_emoji()
+        await self.game.spawn_new()
+        BoardString = await self.game.number_to_emoji()
 
         await interaction.message.edit_message(content=BoardString)
 
@@ -43,7 +44,7 @@ class BetaTwenty48(Twenty48):
         self.board[random.randrange(4)][random.randrange(4)] = 2
 
         for button in self._controls:
-            self.view.add_item(Twenty48_Button(button))
+            self.view.add_item(Twenty48_Button(self, button))
         
         BoardString = await self.number_to_emoji()
         self.message = await ctx.send(content=BoardString, view=self.view, **kwargs)
