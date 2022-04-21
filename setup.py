69b0318e4fa94848d8963eb587fc226d53d7b5ed
Tom-the-Bomb <thello522@gmail.com>
2022-04-21 19:21:48 -0400

    update setup, test

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ with open("Discord_Games/__init__.py") as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 setup(
-    name         = "Discord_Games", 
-    author       = "Tom-the-Bomb", 
-    version      = version, 
-    description  = "A library to help users easily implement games within their discord bot",
-    long_description              = open("README.md").read(),
+    name="Discord_Games", 
+    author="Tom-the-Bomb", 
+    version= version, 
+    description="A library to help users easily implement games within their discord bot",
+    long_description=open("README.md").read(),
     long_description_content_type = "text/markdown",
-    license      = "MIT",
-    url          = "https://github.com/Tom-the-Bomb/Discord-Games",
-    project_urls = {
-        "Repository"   : "https://github.com/Tom-the-Bomb/Discord-Games",
+    license="MIT",
+    url="https://github.com/Tom-the-Bomb/Discord-Games",
+    project_urls={
+        "Repository": "https://github.com/Tom-the-Bomb/Discord-Games",
         "Issue tracker": "https://github.com/Tom-the-Bomb/Discord-Games/issues",
     },
-    classifiers  = [
+    classifiers=[
         "Intended Audience :: Developers",
         'Programming Language :: Python',
         'Natural Language :: English',
@@ -28,9 +28,10 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities'
     ],
-    include_package_data = True,
-    packages             = ['Discord_Games'],
-    install_requires     = ['discord.py', 'english-words', 'chess', 'akinator.py', 'Pillow',],
-    zip_safe        = True,
-    python_requires = '>=3.7'
+    include_package_data=True,
+    package_data={'': ['data/*.png', 'assets/*.txt']}
+    packages=['Discord_Games'],
+    install_requires=['discord.py', 'english-words', 'chess', 'akinator.py', 'Pillow',],
+    zip_safe=True,
+    python_requires='>=3.7'
 )
