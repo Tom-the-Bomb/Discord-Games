7a2bd456d08672bd4a3234ac09e01952a372b7e1
Tom-the-Bomb <thello522@gmail.com>
2022-04-24 18:44:44 -0400

    added memory game!!

---
+++ Discord_Games/button_games/memory_game.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+
+from typing import Optional
+import random
+import asyncio
+
+import discord
+from discord.ext import commands
+
+class MemoryButton(discord.ui.Button):
+    view: MemoryView
+
+    def __init__(self, emoji: str, *, row: int = 0) -> None:
+        self.value = emoji
+
+        super().__init__(
+            label='\u200b',
+            style=self.view.button_style,
+            row=row,
+        )
+
+    async def callback(self, interaction: discord.Interaction) -> None:
+        
+        if opened := self.view.opened:
+            self.emoji = self.value
+            self.disabled = True
+            await interaction.response.edit_message(view=self.view)
+
+            if opened.value != self.value:
+                await asyncio.sleep(self.view.pause_time)
+
+                opened.emoji = None
+                opened.disabled = False
+
+                self.emoji = None
+                self.disabled = False
+                self.view.opened = None
+
+                return await interaction.message.edit(view=self.view)
+            else:
+                self.view.opened = None
+
+                if all(button.disabled for button in self.view.children):
+                    return await interaction.message.edit(content='Game Over, Congrats!', view=self.view)
+        else:
+            self.emoji = self.value
+            self.view.opened = self
+            self.disabled = True
+            return await interaction.response.edit_message(view=self.view)
+
+class MemoryView(discord.ui.View):
+    board: list[list[str]]
+    DEFAULT_ITEMS = ['🥝', '🍓', '🍹', '🍋', '🥭', '🍎', '🍊', '🍍', '🍑', '🍇']
+    
+    def __init__(
+        self, 
+        items: list[str], 
+        *, 
+        button_style: discord.ButtonStyle,
+        pause_time: float,
+        timeout: float = None,
+    ) -> None:
+
+        super().__init__(timeout=timeout)
+
+        self.button_style = button_style
+        self.pause_time = pause_time
+        self.opened: Optional[MemoryButton] = None
+        
+        if not items:
+            items = self.DEFAULT_ITEMS
+        assert len(items) == 10
+
+        items = items * 2
+        random.shuffle(items)
+
+        self.board = [items[i:i + 5] for i in range(0, len(items), 5)]
+
+        for i, row in enumerate(self.board):
+            for item in row:
+                self.add_item(MemoryButton(item, row=i))
+
+class MemoryGame:
+
+    async def start(
+        self, 
+        ctx: commands.Context,
+        *,
+        items: list[str] = [],
+        pause_time: float = 0.7,
+        button_style: discord.ButtonStyle = discord.ButtonStyle.red,
+        timeout: float = None,
+    ) -> discord.Message:
+
+        view = MemoryView(
+            items=items, 
+            button_style=button_style, 
+            pause_time=pause_time,
+            timeout=timeout
+        )
+        return await ctx.send(view=view)
