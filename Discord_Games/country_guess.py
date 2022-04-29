aebc85b7c34d1c6827186defc87b863c90b88548
Tom-the-Bomb <thello522@gmail.com>
2022-04-28 21:03:21 -0400

    fixed more cg stuff

--- Discord_Games/country_guess.py
+++ Discord_Games/country_guess.py
@@ -11,27 +11,32 @@ from discord.ext import commands
 class CountryGuesser:
     country: str
 
-    def __init__(self) -> None:
-        self.has_hint: bool = True
-        self.guesses: int = 5
+    def __init__(self, *, guesses: int = 5, hint: bool = True) -> None:
+        self.has_hint = hint
+        self.guesses = guesses
 
     def get_blanks(self) -> str:
         return ' '.join('_' if char != ' ' else ' ' for char in self.country)
 
     def get_hint(self) -> str:
-        blanks = self.get_blanks()
-        times = int(len(blanks.split()) / 3)
-        blanks = list(blanks)
+        blanks = ['_' if char != ' ' else ' ' for char in self.country]
+        times = round(len(blanks) / 3)
 
         for _ in range(times):
             idx = random.choice(range(len(self.country)))
             blanks[idx] = self.country[idx]
-        return ''.join(blanks)
+        return ' '.join(blanks)
 
     def get_accuracy(self, guess: str) -> int:
         return round(difflib.SequenceMatcher(None, guess, self.country).ratio() * 100)
 
-    async def wait_for_response(self, ctx: commands.Context, *, options: tuple[str, ...] = (), length: Optional[int] = None) -> Optional[tuple[discord.Message, str]]:
+    async def wait_for_response(
+        self, 
+        ctx: commands.Context, 
+        *, 
+        options: tuple[str, ...] = (), 
+        length: Optional[int] = None
+    ) -> Optional[tuple[discord.Message, str]]:
 
         def check(m: discord.Message) -> bool:
             if length:
@@ -72,7 +77,7 @@ class CountryGuesser:
             msg, response = await self.wait_for_response(ctx, length=len(self.country))
 
             if response == self.country:
-                return await msg.reply(f'That is correct! The country was {self.country.title()}')
+                return await msg.reply(f'That is correct! The country was `{self.country.title()}`')
             else:
                 self.guesses -= 1
                 acc = self.get_accuracy(response)
@@ -90,4 +95,4 @@ class CountryGuesser:
                     else:
                         await hint_msg.reply(f'Okay continue guessing! You have **{self.guesses}** guesses left.', mention_author=False)
         
-        return await msg.reply(f'Game Over! you lost, The country wwas `{self.country}`')
+        return await msg.reply(f'Game Over! you lost, The country was `{self.country.title()}`')
